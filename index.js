6bf39c4a7144f9454425fd7429b852a6b6afb889
bredele <olivier@petrofeed.com>
2013-09-04 18:19:22 -0600

    first draft is not workin, it's not perfect and the code could be better

--- index.js
+++ index.js
@@ -1,6 +1,117 @@
+var Binding = require('data-binding');
+var Store = require('store');
+
 
 /**
  * Expose 'event-plugin'
  */
 
 module.exports = Plugin;
+
+
+/**
+ * Plugin constructor.
+ * @param {Object} model (should have getter/setter and inherit from emitter)
+ * @api public
+ */
+
+function Plugin(store){
+  this.store = store;
+  this.items = [];
+}
+
+
+/**
+ * Each util.
+ * Iterate through store.
+ * @param  {HTMLElement} node 
+ * @api public
+ */
+
+Plugin.prototype.each = function(node) {
+  var data = this.store.data;
+  var first = node.firstChild;
+  var _this = this;
+  this.node = node;
+  //NOTE: may be instead that get the string of node and pass to the renderer
+  //do benchmark
+  this.clone = first.cloneNode(true);
+
+  node.removeChild(first);
+
+
+  this.store.on('change', function(key, value){
+    var item = _this.items[key];
+    if(item) {
+      _this.items[key].reset(value); //do our own emitter to have scope
+    } else {
+      //create item renderer
+      _this.addItem(value);
+    }
+  });
+
+  this.store.on('deleted', function(key){
+    var item = _this.items.splice(key, key)[0]; //remove from array...is there a better way?
+    item.unbind(node);
+    item = null; //for garbage collection
+  });
+
+  //NOTE: might be in store (store.loop)
+  for(var i = 0, l = data.length; i < l; i++){
+    this.addItem(data[i]);
+  }
+};
+
+/**
+ * Create item renderer from data.
+ * @param  {Object} data 
+ * @api private
+ */
+
+Plugin.prototype.addItem = function(data) {
+  var item = new ItemRenderer(this.clone, data);
+  this.items.push(item);
+  this.node.appendChild(item.dom);
+};
+
+
+/**
+ * Item renderer.
+ * Represents the item that going to be repeated.
+ * @param {HTMLElement} node 
+ * @param {Store} data 
+ * @api private
+ */
+
+function ItemRenderer(node, data){
+  //NOTE: is it more perfomant to work with string?
+  this.dom = node.cloneNode(true);
+  this.store = new Store(data);
+  this.binding = new Binding(this.store); //we have to have a boolean parameter to apply interpolation &|| plugins
+  this.binding.apply(this.dom);
+}
+
+
+/**
+ * Unbind an item renderer from its ancestor.
+ * @param  {HTMLElement} node 
+ * @api private
+ */
+
+ItemRenderer.prototype.unbind = function(node) {
+  //remove all listener and delete data on store
+  //+
+  node.removeChild(this.dom);
+};
+
+
+/**
+ * Reset iten renderer.
+ * @param  {Object} data 
+ * @api private
+ */
+
+ItemRenderer.prototype.reset = function(data) {
+  this.store.reset(data);
+};
+
